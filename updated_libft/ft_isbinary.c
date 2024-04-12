1163b6ad2df928a92840c1285dbe0b6461a9cae1
Alex Schenk <alexander.o.schenk@gmail.com>
2024-04-12 15:32:48 +0200

    whitespace check included

--- updated_libft/ft_isbinary.c
+++ updated_libft/ft_isbinary.c
@@ -6,17 +6,17 @@
 /*   By: aschenk <aschenk@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 19:35:30 by aschenk           #+#    #+#             */
-/*   Updated: 2024/01/20 19:46:18 by aschenk          ###   ########.fr       */
+/*   Updated: 2024/04/12 15:31:32 by aschenk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 To handle the reading of binary data in a more controlled way, ft_isbinary()
-checks if the 'stash' contains non-printable characters and NULL terminators not
-followed by another NULL terminator, which indicates an end-of-file (EOF).
-If the binary data check was successful (i.e., if non-printable characters or a
-single NULL terminator was found without an EOF indication), '1' is returned,
-otherwise '0'.
+checks if the 'stash' contains non-whitespace non-printable characters and
+NULL terminators not followed by another NULL terminator, which indicates an
+end-of-file (EOF). If the binary data check was successful (i.e., if
+non-printable characters or a single NULL terminator was found without an EOF
+indication), '1' is returned, otherwise '0'.
 */
 
 #include "libft.h"
@@ -28,9 +28,10 @@ int	ft_isbinary(char *stash)
 	i = 0;
 	while (stash[i] && stash[i] != '\n')
 	{
-		if (stash[i] == '\0' || stash[i] < 32 || stash[i] > 126)
+		if (stash[i] == '\0' || !ft_isprint(stash[i]))
 		{
-			if (stash[i] == '\0' && stash[i + 1] == '\0')
+			if ((stash[i] == '\0' && stash[i + 1] == '\0')
+				|| (stash[i] >= 9 && stash[i] <= 13))
 				break ;
 			else
 				return (1);
